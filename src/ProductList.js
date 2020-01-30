e82ca5b44f57c341bf329b0a4f943fcb6aa8c44e
emresert <emresert1233@gmail.com>
2020-01-30 13:31:56 +0300

    ProductList.js is completed

--- src/ProductList.js
+++ src/ProductList.js
@@ -30,7 +30,7 @@ export default class ProductList extends Component {
                             ))}
                     </tbody>
                 </Table>
-
+ {/** */}
 
             </div>
         )
