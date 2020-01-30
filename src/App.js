b9f93e17033447be4be48503c879feaf7ba82d69
emresert <emresert1233@gmail.com>
2020-01-30 12:38:20 +0300

    AlertifyJS is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button } from 'reactstrap'; 
 
+
+
+
 function App() {
+  
+
   return (
+    
     <div className="App">
+      
     <Button color="danger mt-5">Danger!</Button>
     </div>
   );
