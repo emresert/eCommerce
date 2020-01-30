d62cd629de8ab1eb87b4e644cc1fc3a104fe026b
emresert <emresert1233@gmail.com>
2020-01-30 13:29:17 +0300

    App.js part is completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,106 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'reactstrap'; 
+import Navi from './Navi';
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
+import { Container, Row, Col } from 'reactstrap';
+import { Component } from 'react'
+
+import alertify from "alertifyjs";
 
 
+export default class App extends Component {
+
+  state = {
+    currentCategory: "",
+    categories: [],
+    products: [],
+    apiUrl: "http://localhost:3000/",
+    cart:[]
+  }
 
 
-function App() {
+  changeCurrentCategory = dataForChange => {
+    this.setState({ currentCategory: dataForChange.categoryName });
+  }
   
+  getCategories = () => {
+    fetch(this.state.apiUrl + "categories")
+      .then(res => res.json())
+      .then(data => this.setState({ categories: data }));
+  }
 
-  return (
-    
-    <div className="App">
+  getCategoryId = (catId) => {
+    this.getProducts(catId);
+  }
+
+  getProducts = (categoryId) => {
+    if (categoryId) {
+      fetch(this.state.apiUrl + "products/?categoryId=" + categoryId).then(res => res.json()).then(
+        data => this.setState({ products: data }))
+    }
+    else {
+
+      fetch(this.state.apiUrl + "products").then(res => res.json()).then(
+        data => this.setState({ products: data }))
+    }
+  }
+
+  addToCart = (_product) =>{
+     let newCart = this.state.cart;
+     let addedItem =newCart.find(p=>p.product.id === _product.id)
       
-    
-    </div>
-  );
+     if (addedItem) {
+       addedItem.quantity = addedItem.quantity + 1 ;
+     }
+     else{
+      newCart.push({product:_product,quantity:1});
+     }
+
+     this.setState({cart:newCart});
+     alertify.success(_product.productName + " is added to cart")
+  }
+
+  removeItemFromCart =(_product)=>{
+    let newCart= this.state.cart.filter(p=>p.product.id !== _product.id );
+    this.setState({cart:newCart});
+  }
+
+  componentDidMount() {
+    this.getCategories();
+    this.getProducts();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Container>
+
+          <Navi removeItemFromCart={this.removeItemFromCart} cartItems={this.state.cart}></Navi>
+
+          <Row>
+
+            <Col xs="3">
+              <CategoryList
+                categories={this.state.categories}
+                getCategoryId={this.getCategoryId}
+                currentCategory={this.state.currentCategory}
+                changeCurrentCategory={this.changeCurrentCategory} >
+              </CategoryList>
+            </Col>
+
+            <Col xs="9">
+              
+              <ProductList products={this.state.products} addToCart = {this.addToCart} >
+
+              </ProductList>
+            </Col>
+
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
 
-export default App;
+
