fa52766d54c1eaa2d69251c397ad7f8af30798b3
emresert <emresert1233@gmail.com>
2020-01-30 13:32:30 +0300

    CategoryList.js is completed

---
+++ src/CategoryList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
+
+export default class CategoryList extends Component {
+
+ 
+    render() {
+        return (
+            <div className="catLeftList">
+
+                 
+                <ListGroup>
+                    {this.props.categories.map(data => (
+                        <ListGroupItem 
+                            active={data.categoryName === this.props.currentCategory?true:false}
+                            key={data.id}
+                            onClick={() => {this.props.changeCurrentCategory(data);this.props.getCategoryId(data.id)}
+                                }
+                        >
+                            {data.categoryName}
+                        </ListGroupItem>
+                    ))}
+                </ListGroup>
+
+                
+
+            </div>
+        )
+    }
+}
